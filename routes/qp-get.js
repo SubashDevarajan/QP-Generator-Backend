3f73a237b568f40d4a0ba7acbdd3e23379c31c21
YogeshThiru01 <61374054+YogeshThiru01@users.noreply.github.com>
2022-06-16 15:42:41 +0530

    modification added for specific user

--- routes/qp-get.js
+++ routes/qp-get.js
@@ -2,10 +2,13 @@ import express from "express";
 import pool from "../db.js";
 
 const getqp = express();
-getqp.get("/qp", async (req, res) => {
+getqp.get("/qp/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    console.log("qp details get");
-    const qp = await pool.query("Select * from qpdetails");
+    console.log("qp details get for specific user");
+    const qp = await pool.query("Select * from qpdetails where user_id=$1", [
+      id,
+    ]);
     res.json(qp);
   } catch (error) {
     res.status(401).json({ error: error.message });
